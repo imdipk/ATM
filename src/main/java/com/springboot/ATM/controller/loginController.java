36e8aeb909765325ff691004bd47b872c0ee7fb5
Deepak Jangir <jangirdeepak121@gmail.com>
2022-11-21 11:37:15 +0530

    login page added

---
+++ src/main/java/com/springboot/ATM/controller/loginController.java
@@ -0,0 +1,17 @@
+package com.springboot.ATM.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class loginController {
+
+	
+	@GetMapping("/login")
+		public String openHome() {
+			return "login";
+		}
+	}
+
+
